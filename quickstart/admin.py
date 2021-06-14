d187143213e91a9e30d7b219e8e29009ab78d560
NiWaRe <n.remerscheid@bluewin.ch>
2021-06-14 13:03:34 +0200

    add new models to admin interface

--- quickstart/admin.py
+++ quickstart/admin.py
@@ -3,6 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import (
     Product,
+    Doctors, 
+    Patients, 
+    Inventory, 
+    PrescriptionTypes, 
+    Prescriptions, 
+    UserOwnsMedication, 
 )
 
-admin.site.register(Product)
+admin.site.register(
+    Product, 
+    Doctors, 
+    Patients, 
+    Inventory, 
+    PrescriptionTypes, 
+    Prescriptions, 
+    UserOwnsMedication, 
+)
