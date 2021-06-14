5e3a4b9acc3827fed4aee96f05442ecad0b22da5
NiWaRe <n.remerscheid@bluewin.ch>
2021-06-14 13:04:55 +0200

    add new models to admin interface

--- quickstart/admin.py
+++ quickstart/admin.py
@@ -11,12 +11,10 @@ from .models import (
     UserOwnsMedication, 
 )
 
-admin.site.register(
-    Product, 
-    Doctors, 
-    Patients, 
-    Inventory, 
-    PrescriptionTypes, 
-    Prescriptions, 
-    UserOwnsMedication, 
-)
+admin.site.register(Product)
+admin.site.register(Doctors)
+admin.site.register(Patients)
+admin.site.register(Inventory)
+admin.site.register(PrescriptionTypes)
+admin.site.register(Prescriptions)
+admin.site.register(UserOwnsMedication)
