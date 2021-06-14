11f538426bfdd7c72cc703cecce2d1456f24fabc
NiWaRe <n.remerscheid@bluewin.ch>
2021-06-14 12:11:27 +0200

    change to postgresDB

--- medicaite/settings.py
+++ medicaite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'medicaite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medicaite_database',
+        'USER': 'medicaite_admin',
+        'PASSWORD': 'medicaite21',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
